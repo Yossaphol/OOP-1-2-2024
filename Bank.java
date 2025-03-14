eb06553bd2122255c32581f8e3e8eba72ab0d3bc
kennichiwa <69375434+Yossaphol@users.noreply.github.com>
2025-03-14 14:09:48 +0700

    lab10

--- Bank.java
+++ Bank.java
@@ -1,32 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Test;
-import java.util.*;
-/**
- *
- * @author khaopad
- */
 public class Bank {
-    public static void main(String[] args) {
-        double money, result = 0;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input your money : ");
-        money = input.nextDouble();
-        System.out.print("Input your account type (Please type A B C or X in uppercase) : ");
-        char type = input.next().charAt(0);
-        switch (type) {
-            case 'A': case 'C':
-                result = money + (money * 1.5 / 100);
-                break;
-            case 'B':
-                result = money + (money * 2 / 100);
-                break;
-            case 'X':
-                result = money + (money * 5 / 100);
-                break;
+    private Account acct[];
+    private int numAcct;
+    
+    public Bank() {
+        acct = new Account[10];
+        numAcct = 0;
+    }
+    
+    public void addAccount(Account ac) {
+        if (numAcct < acct.length) {
+            acct[numAcct] = ac;
+            numAcct += 1;
         }
-        System.out.println("Your total money in one year = "+ result);
+    }
+    public Account getAccount(int index) {
+        return acct[index];
+    }
+    public int getNumAcct() {
+        return numAcct;
     }
 }
